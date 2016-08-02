7a1ea3beb419380ba3754a074f7ec1c627e242dd
pendgaft <m.schuchard@gmail.com>
2016-08-02 14:26:00 -0400

    first commit

---
+++ memRank.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python3
+
+import re
+import subprocess
+
+MEM_SIZE = re.compile("([0-9]+)\\s+.B")
+
+def main(hostFile):
+    memResults = {}
+    ulimResults = {}
+    hostFileFP = open(hostFile, "r")
+    hosts = []
+    for line in hostFileFP:
+        line = line.strip()
+        if len(line) > 0:
+            hosts.append(line)
+    hostFileFP.close()
+    print("starting querries")
+    counter = 0
+    nextPoint = len(hosts) / 10
+    for host in hosts:
+        sizeKB = queryMem(host)
+        if sizeKB > 0:
+            memResults[host] = sizeKB / (1024.0 * 1024.0)
+        counter = counter + 1
+        if counter == nextPoint:
+            print(str(float(counter) / float(len(hosts))) + "% done")
+            nextPoint = nextPoint + len(hosts) / 10
+    print("done with querries, starting sorting")
+    top200Mem = topN(memResults, 200)
+    outFP = open("high-mem.txt", "w")
+    printList(top200Mem, memResults, outFP)
+    outFP.close()
+
+def printList(incList, incMap, outFP=None):
+    for tItem in incList:
+        if outFP == None:
+            print(tItem + "," + str(incMap[tItem]))
+        else:
+            outFP.write(tItem + "," + str(incMap[tItem]) + "\n")
+            
+def topN(incMap, n):
+    if len(incMap) < n:
+        n = len(incMap)
+    topList = []
+    while len(topList) < n:
+        currentTop = None
+        currentVal = 0.0
+        for tKey in incMap:
+            if (tKey not in topList) and incMap[tKey] > currentVal:
+                currentVal = incMap[tKey]
+                currentTop = tKey
+        topList.append(currentTop)
+    return topList
+
+def execCmd(host, cmd):
+    result = []
+    try:
+        sshProc = subprocess.run(["ssh", "-l", "umn_hopper", "-o", "StrictHostKeyChecking=no", host, cmd], stdout = subprocess.PIPE, timeout=2, universal_newlines=True)
+        for line in sshProc.stdout:    
+            result.append(line)
+    except subprocess.TimeoutExpired:
+        print("timeout")
+        return None
+    except subprocess.SubprocessError:
+        print("err")
+        return None
+    return "".join(result).split("\n")
+
+def queryUlimit(host):
+    ulimitResult = execCmd(host, "ulimit -n")
+    for line in ulimitResult:
+        line = line.strip()
+        if line.isnumeric():
+            return int(line)
+    return -1
+
+def queryMem(host):
+    memResult = execCmd(host, "cat /proc/meminfo")
+    if memResult is None:
+        return -1
+    for line in memResult:
+        if "MemFree" in line:
+            matcher = MEM_SIZE.search(line)
+            if matcher:
+                return float(matcher.group(1))
+    return -1
+
+
+if __name__ == "__main__":
+    main("current-nodes.txt")
---
+++ origDL_nodes.py
@@ -0,0 +1,128 @@
+#!/usr/bin/python
+"""
+download_logs.py
+
+@author Brian Sanderson
+@date 1/2007
+
+Downloads boot nodes into slice and generates node lists.
+"""
+
+import sys
+import xmlrpclib
+import sets
+
+remove_nbnodes = '-r' in sys.argv
+no_file_writes = '-n' in sys.argv
+
+api_server = xmlrpclib.Server('https://www.planet-lab.org/PLCAPI/')
+
+# in Python, this creates a dictionary, or associative array.
+auth= {}
+auth['Username']= "yourlogin@byu.edu"
+# always 'password', for password based authentication
+auth['AuthMethod']= "password"
+auth['AuthString']= "yourpassword"
+# valid roles include, 'user', 'tech', 'pi', 'admin'
+auth['Role']= "user"
+slice_name = 'yourslicename'
+
+# use the AdmAuthCheck function to make sure we
+# are authorized and can make api calls
+print "Authorizing Planet-lab ... ",
+sys.stdout.flush()
+authorized = api_server.AdmAuthCheck(auth)
+if authorized:
+	print "[SUCCESS]"
+else:
+	print "[FAILURE] Permission denied."
+	sys.exit()
+
+return_fields = ['hostname','boot_state']
+
+# get the all the nodes, regardless of current boot state
+print "Retrieving node lists ...",
+sys.stdout.flush()
+all_nodes = api_server.GetNodes(auth, {}, ['node_id', 'hostname', 'boot_state'])
+have_node_ids = api_server.GetSlices(auth, [ slice_name ], ['node_ids'])[0]['node_ids']
+have_nodes = [node['hostname'] for node in api_server.GetNodes(auth, have_node_ids, ['hostname'])]
+print "done."
+
+toadd_nodes = []
+todel_nodes = []
+
+# nodes is an array of associative arrays/dictionary objects, each one
+# corresponding to a node returned. the keys in each array element are
+# the keys specified in return_fields
+
+for node_record in all_nodes:
+	if (node_record['hostname'] not in have_nodes) and node_record['boot_state'] == 'boot':
+		toadd_nodes.append(node_record['hostname'])
+	elif (node_record['hostname'] in have_nodes) and node_record['boot_state'] != 'boot':
+		todel_nodes.append(node_record['hostname'])
+
+print "Found %d new boot node(s)" % (len(toadd_nodes))
+print "Found %d node(s) no longer in boot state" % (len(todel_nodes))
+
+if len(toadd_nodes) > 0:
+	# add node to slice
+	print "Adding new nodes to slice ...",
+	sys.stdout.flush()
+	result = api_server.AddSliceToNodes(auth, slice_name, toadd_nodes)
+	if result == 1:
+		print "SUCCESS"
+	else:
+		print "FAILED!"
+	sys.stdout.flush()
+
+	fp = open('new-nodes.txt','w')
+	for node in toadd_nodes:
+		fp.write("%s\n" % (node));
+	fp.close()
+
+if remove_nbnodes and len(todel_nodes) > 0:
+	#remove nodes from slice
+	print "Removing non-boot nodes from slice ...",
+	sys.stdout.flush()
+	result = api_server.SliceNodesDel(auth, slice_name, todel_nodes)
+	if result == 1:
+		print "SUCCESS"
+	else:
+		print "FALIED!"
+	sys.stdout.flush()
+
+if not no_file_writes:
+	print "Writing out all node list ...",
+	sys.stdout.flush()
+	
+	# find out current nodes
+	merged_nodes = sets.Set()
+	afp = open('all-nodes.txt', 'r')
+	for node in afp:
+		merged_nodes.add(node.strip())
+	afp.close()
+
+	# add in all found nodes
+	for node in all_nodes:
+		merged_nodes.add(node['hostname'].strip())
+	
+	# write out all node list
+	afp = open('all-nodes.txt','w')
+	for node in merged_nodes:
+	        afp.write("%s\n" % (node))
+	afp.close()
+	print "done."
+
+	# write out current node list
+	print "Writing out current node list ...",
+	sys.stdout.flush()
+
+	have_node_ids = api_server.GetSlices(auth, [ slice_name ], ['node_ids'])[0]['node_ids']
+	have_nodes = [node['hostname'] for node in api_server.GetNodes(auth, have_node_ids, ['hostname'])]
+	
+	cfp = open('current-nodes.txt','w')
+	for node in have_nodes:
+	        cfp.write("%s\n" % (node))
+	cfp.close()
+	print "done."
+
